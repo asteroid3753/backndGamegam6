6557771ddf0a209557b506ec9fd05691f62cea6b
silrider <wlsmdltn@gmail.com>
2023-12-16 08:14:42 +0900

    Add SoundManager, Add OdinInspector

---
+++ Assets/02_Scripts/JinEuiSoo/SoundManager/Additional/SimpleAmbienceSoundCreator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace MorningBird.Sound
+{
+    public class SimpleAmbienceSoundCreator : SerializedMonoBehaviour
+    {
+        [FoldoutGroup("PreDefine")]
+        [SerializeField] AudioStorage _ambienceSound;
+
+        [FoldoutGroup("Debug")]
+        [SerializeField] BasicSoundClipPlay_Common _soundCommon;
+
+
+        private void OnEnable()
+        {
+            if(_soundCommon != null)
+            {
+                _soundCommon.ReturnToPool();
+            }
+
+            _soundCommon = SoundManager.Instance.RequestPlayAmbience(_ambienceSound, this.transform.position, isLoop: true, setFollowTarget: this.transform);
+
+        }
+
+        private void OnDisable()
+        {
+            if (_soundCommon == null)
+                return;
+
+            try
+            {
+                _soundCommon.ReturnToPool();
+
+            }
+            catch (System.NullReferenceException)
+            {
+
+            }
+
+        }
+
+        private void OnDestroy()
+        {
+            if (_soundCommon == null)
+                return;
+
+            try
+            {
+                _soundCommon.ReturnToPool();
+
+            }
+            catch(System.NullReferenceException)
+            {
+
+            }
+
+        }
+    }
+}
