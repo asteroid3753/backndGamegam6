5bc42599c77ee015a431cb83607f08bd040db665
ooo1324 <ooo1324@daum.net>
2023-12-16 11:18:55 +0900

    Fix : Recv Event 오류  수정
    - Recv Event UnityEvent에서 Action Event로 수정

---
+++ Assets/02_Scripts/KimSoYeon/RecvTest.cs
@@ -0,0 +1,27 @@
+using BackEnd.Tcp;
+using KSY;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecvTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // recv 이벤트 등록
+        BackEndManager.Instance.Parsing.PlayerMoveEvent += PlayerMoveRecvFunc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayerMoveRecvFunc(SessionId sessionId, Vector2 vec)
+    {
+        // 들어오면 
+        Debug.Log($"x : {vec.x}, y : {vec.y}");
+    }
+}
