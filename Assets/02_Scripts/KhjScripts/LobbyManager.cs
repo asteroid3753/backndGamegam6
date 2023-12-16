e6bfa481f60fb4a9260401b9bb7dbbcd233321d6
khj <asteroid3753@naver.com>
2023-12-16 10:34:45 +0900

    Feat: Lobby

---
+++ Assets/02_Scripts/KhjScripts/LobbyManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+using BackEnd.Game;
+using UnityEngine.UI;
+using TMPro;
+
+namespace khj
+{
+    public class LobbyManager : MonoBehaviour
+    {
+        [SerializeField] GameObject titlePanel;
+        [SerializeField] GameObject loginPanel;
+        [SerializeField] GameObject matchPanel;
+        [SerializeField] GameObject matchManager;
+        [SerializeField] GameObject roomPanel;
+        [SerializeField] TMP_InputField nameInputField;
+        string nickName;
+        void Awake()
+        {
+            titlePanel.SetActive(true);
+            loginPanel.SetActive(false);
+        }
+
+        private void Start()
+        {
+            var bro = Backend.Initialize();
+        }
+
+        public void TouchToStart()
+        {
+            titlePanel.SetActive(false);
+            loginPanel.SetActive(true);
+        }
+        public void MatchStart()
+        {
+            loginPanel.SetActive(false);
+            matchPanel.SetActive(true);
+            matchManager.SetActive(true);
+        }
+
+        public void Login()
+        {
+            nickName = nameInputField.text;
+            var bro = Backend.BMember.CustomLogin(nickName, nickName);
+
+            if (bro.IsSuccess())
+            {
+                Debug.Log("로그인 : " + bro);
+                MatchStart();
+            }
+            else if (bro.GetStatusCode() == "401")
+            {
+                SignUp();
+                MatchStart();
+            }
+            else
+            {
+                Debug.LogError("로그인 : " + bro);
+            }
+        }
+
+        void SignUp()
+        {
+            var bro = Backend.BMember.CustomSignUp(nickName, nickName);
+
+            if (bro.IsSuccess())
+            {
+                Debug.Log("회원가입: " + bro);
+                UpdateNickname();
+            }
+            else
+            {
+                Debug.LogError("회원가입: " + bro);
+            }
+        }
+
+        public void UpdateNickname()
+        {
+            var bro = Backend.BMember.UpdateNickname(nickName);
+
+            if (bro.IsSuccess())
+            {
+                Debug.Log("닉네임 변경 : " + bro);
+            }
+            else
+            {
+                Debug.LogError("닉네임 변경 : " + bro);
+            }
+        }
+    }
+}
